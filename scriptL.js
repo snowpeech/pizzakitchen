2bbb4a52369a1263940ee798f86af7ccd2d7bd91
snowpeech <snowpeech@gmail.com>
2019-05-08 16:00:37 -0500

    new cashier image and stop function works

--- scriptL.js
+++ scriptL.js
@@ -8,6 +8,7 @@
 
 
 		var intervalId;
+		var cashierInterval;
 
 		// reset();
 
@@ -46,6 +47,7 @@
 			customer.style.left = `${parseInt(customer.style.left.replace("px",""))+10}px`
 		}
 
+
 		function customerEvalStop(){
 			var customerLeft = parseInt(customer.style.left.replace("px",""));
 			var customerRight = customerLeft + 83;
@@ -63,6 +65,7 @@
         	audioOrder();
         	billAppear();
         	cashierStart();
+        	cashierEvalStop();
         	
         });
 
@@ -72,37 +75,44 @@
         }
 
         function billAppear(){
-        	var receipt = document.getElementById("receipt");
         	receipt.style.display="block";
+        	cashier.style.transform = "scaleX(-1)";
         	console.log("bill Appears")
         }
 
         function cashierStart() {
-			intervalId = setInterval(cashierTick, 50);
+			cashierInterval = setInterval(cashierTick, 50);
         }
 
         function cashierTick() {
 			cashierWalk();
 			cashierEvalStop();
+			cashierEvalReturn();
         }
 
         function cashierWalk(){
-        	
+        	//cashier needs to flip direction
         	cashier.style.left = `${parseInt(cashier.style.left.replace("px",""))+10}px`
         	receipt.style.left = `${parseInt(receipt.style.left.replace("px",""))+10}px`
-        	//console.log("cashierWalk")
         }
-/// ERRORS BELOW!!!! VIEWPORT SIZE ISSUES ON STOPPING
+
+        function cashierStop() {
+        	clearInterval(cashierInterval);
+        }
+/// ERRORS BELOW!!!! cashierLeft isnt updating
         function cashierEvalStop(){
-        	var cashierLeft = parseInt(customer.style.left.replace("px",""));
-        	var cashierStop = parseInt(viewportSize.getWidth())-100;
-        	console.log(cashierStop)
+        	var cashierLeft = parseInt(cashier.style.left.replace("px",""));
+        	
+        	const cashierStopPt = 500; //change once kitchen is updated
+        	console.log(cashierStop); 
 
-        	console.log("cashier stop!")
+        	if (cashierLeft >= cashierStopPt) {
+				cashierStop();
 
-        	if (cashierLeft >= cashierStop) {
-				stop();
-				
-				console.log("cashier stop")
 			}
+        }
+
+        function cashierEvalReturn(){
+        	//call coordinates of interest
+        	//when cashier is near chef, bill disappears and 
         }
