45cd7ab363b1be48201a915f7365e925e9e6f7a0
snowpeech <snowpeech@gmail.com>
2019-05-07 22:00:33 -0500

    working on the cashier stopping

--- scriptL.js
+++ scriptL.js
@@ -4,6 +4,8 @@
 		var customer = document.getElementById('customer');
 		var orderTable = document.getElementById('order-table');
 		var cashier = document.getElementById('cashier');
+		var receipt = document.getElementById("receipt");
+
 
 		var intervalId;
 
@@ -28,19 +30,19 @@
 		///////
  
 		function start() {
-			intervalId = setInterval(tick, 50);
+			intervalId = setInterval(customerTick, 50);
         }
 
-        function tick() {
-			moveRight();
+        function customerTick() {
+			customerMoveRight();
 			customerEvalStop();
         }
 
-		function stop(){
+		function customerStop(){
 			clearInterval(intervalId);
 		}
 
-		function moveRight() {
+		function customerMoveRight() {
 			customer.style.left = `${parseInt(customer.style.left.replace("px",""))+10}px`
 		}
 
@@ -50,16 +52,57 @@
 			var tableLeft = parseInt(orderTable.style.left.replace("px",""));
 
 			if (customerRight >= (tableLeft +2)) {
-				stop();
-				console.log("Triggering the OrderIn event")
-           		document.dispatchEvent(orderIn);
+				customerStop(); //note that customerEvalStop is going to keep running
+           		orderTable.dispatchEvent(orderIn);
 			}
 		}
 
 	//add event listener
-		document.addEventListener("OrderIn", function(e) {
-        	alert("Dog barked event!");
-        	console.log("person speak")
-        	goodBoy();
-        	return false // this keeps the page from reloading
+		orderTable.addEventListener("orderIn", function(e) {
+        	console.log("customer ordered")
+        	audioOrder();
+        	billAppear();
+        	cashierStart();
+        	
         });
+
+        function audioOrder(){
+        	var voiceOrder = document.getElementById("order-up");
+			voiceOrder.play();  
+        }
+
+        function billAppear(){
+        	var receipt = document.getElementById("receipt");
+        	receipt.style.display="block";
+        	console.log("bill Appears")
+        }
+
+        function cashierStart() {
+			intervalId = setInterval(cashierTick, 50);
+        }
+
+        function cashierTick() {
+			cashierWalk();
+			cashierEvalStop();
+        }
+
+        function cashierWalk(){
+        	
+        	cashier.style.left = `${parseInt(cashier.style.left.replace("px",""))+10}px`
+        	receipt.style.left = `${parseInt(receipt.style.left.replace("px",""))+10}px`
+        	//console.log("cashierWalk")
+        }
+/// ERRORS BELOW!!!! VIEWPORT SIZE ISSUES ON STOPPING
+        function cashierEvalStop(){
+        	var cashierLeft = parseInt(customer.style.left.replace("px",""));
+        	var cashierStop = parseInt(viewportSize.getWidth())-100;
+        	console.log(cashierStop)
+
+        	console.log("cashier stop!")
+
+        	if (cashierLeft >= cashierStop) {
+				stop();
+				
+				console.log("cashier stop")
+			}
+        }
