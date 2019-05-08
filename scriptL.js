b9984ef10957e03967533aa0f696bc350768ec98
snowpeech <snowpeech@gmail.com>
2019-05-08 17:56:13 -0500

    troubleshooting cashier walking back

--- scriptL.js
+++ scriptL.js
@@ -61,8 +61,7 @@
 
 	//add event listener
 		orderTable.addEventListener("orderIn", function(e) {
-        	console.log("customer ordered")
-        	audioOrder();
+        	audioOrder(); //add delay of .5 seconds here?
         	billAppear();
         	cashierStart();
         	cashierEvalStop();
@@ -77,7 +76,6 @@
         function billAppear(){
         	receipt.style.display="block";
         	cashier.style.transform = "scaleX(-1)";
-        	console.log("bill Appears")
         }
 
         function cashierStart() {
@@ -88,22 +86,23 @@
 			cashierWalk();
 			cashierEvalStop();
 			cashierEvalReturn();
+			console.log("cashier tick loop")
         }
 
         function cashierWalk(){
-        	//cashier needs to flip direction
         	cashier.style.left = `${parseInt(cashier.style.left.replace("px",""))+10}px`
         	receipt.style.left = `${parseInt(receipt.style.left.replace("px",""))+10}px`
         }
 
         function cashierStop() {
         	clearInterval(cashierInterval);
+        	console.log("cashier stop fn")
         }
 /// ERRORS BELOW!!!! cashierLeft isnt updating
         function cashierEvalStop(){
         	var cashierLeft = parseInt(cashier.style.left.replace("px",""));
         	
-        	const cashierStopPt = 500; //change once kitchen is updated
+        	const cashierStopPt = 500; //change to chef meeting point once kitchen is updated
 
         	if (cashierLeft >= cashierStopPt) {
 				cashierStop();
@@ -114,15 +113,50 @@
         	//call coordinates of interest (cashierLeft. chefRight)
         	var chef1 = document.getElementById("chef1");
         	var cashierLeft = parseInt(cashier.style.left.replace("px","")); //is this redundant? can I make this a global variable?
-        	console.log("eval return cashier left:", cashierLeft)
+        	var cashierRight = cashierLeft + parseInt(cashier.style.width.replace("px",""));
 
-        	var cashierRight = cashierLeft +parseInt(cashier.style.width.replace("px",""));
         	console.log("cashier right", cashierRight)
 
-        	// var chef1Left = parseInt(chef1.style.left.replace("px",""));
-        	// var chef1Right = chef1Left + 179; //parseInt(chef1.style.width.replace("px",""));
-        	// console.log ("chef1:", chef1)
-        	// console.log("Cashier Left: ",cashierLeft)
-        	// console.log (chef1Right)
-        	//when cashier is near chef, bill disappears and cashier walks back
+        	var chef1Left = 580 //parseInt(chef1.style.left.replace("px",""));
+        	console.log("Cashier Left: ",cashierLeft)
+    
+
+        	if (cashierRight >= (chef1Left +10)){
+        		cashierReturn();
+        	}
+        	//when cashierRight is near chefLeft, bill disappears and cashier walks back
+        }
+
+        function cashierReturn(){
+        	cashierFlip();
+        	cashierWalkLeft();
+        	//cashierEvalReset();
+        }
+
+        function cashierFlip(){
+        	receipt.style.display = "none";
+        	cashier.style.transform = "scaleX(1)";
+        }
+        function cashierWalkLeft(){
+        	cashier.style.left = `${parseInt(cashier.style.left.replace("px",""))+10}px`
+        	console.log("cashier walk left")
+        	console.log(cashier.style.left)//493
+        	//i think this doesn't work because cashierWalkRight is still running
+        }
+
+        function cashierEvalReset(){
+        	//stop once cashierLeft is near tableRight
+        	var cashierLeft = parseInt(cashier.style.left.replace("px",""));
+        	console.log("eval reset cashier L", cashierLeft)
+        	var tableRight = parseInt(orderTable.style.left.replace("px",""))+100; //table Lf + width
+        	console.log("eval reset table R", tableRight)
+
+        	if (cashierLeft <= tableRight) { //and bill display ==none ?
+				console.log("cashier returned")
+				console.log("cashierleft evalrest", cashierLeft)
+				console.log("table R, eval reset", tableRight)
+				
+				cashierStop();
+			}
+
         }
